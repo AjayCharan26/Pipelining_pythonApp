976f7a0f680bbb3959e2e2f6385977da7817092d
VANGAVETI AJAYCHARAN <2320030033@klh.edu.in>
2025-08-26 14:50:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ def index():
         result = predict(user_text)
     rows = read_sample_rows()
     return render_template("index.html", result=result, rows=rows)
-
+    
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
